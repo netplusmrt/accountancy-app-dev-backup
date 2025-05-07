417bf6705458bd4ce630b27570d4270544f22727
Mushrif Ali Khan <mushrifali@gmail.com>
2025-05-07 18:53:44 +0530

    updated exportDataPerUIDAndYear

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const firebaseExport = require("../firebase-admin/export"); 
 
 // Path to your service account key JSON file
-firebaseExport.exportDataPerUIDAndYear('../accountancy-app-dev-firebase-adminsdk-da82p-e37a0a5c28.json').catch(console.error);
+const serviceAccountPath = '../accountancy-app-dev-firebase-adminsdk-da82p-e37a0a5c28.json';
+
+firebaseExport.exportDataPerUIDAndYear(serviceAccountPath).catch(console.error);
