ef2885b958629d02b43ac11901ad2266e70b4e63
Mushrif Ali Khan <mushrifali@gmail.com>
2025-07-06 12:39:27 +0530

    updated updateInvoiceTaxableAmount

--- update-invoice-taxable-amount.js
+++ update-invoice-taxable-amount.js
@@ -1,5 +1,5 @@
 const constants = require('./constants')
 
 // Update Invoice Taxable Amount
-const updateInvoiceTaxableAmount = require("../firebase-admin/updater"); 
+const { updateInvoiceTaxableAmount } = require("../firebase-admin/updater");
 updateInvoiceTaxableAmount(constants.serviceAccountPath).catch(console.error);
